15ecd577dd432c9c88d219a717a6d07aa472e5f5
KevColomaDev <kev.enrri@hotmail.com>
2024-11-22 23:29:04 -0500

    feat: icon & splashscreen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,25 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'examenLibros',
-  webDir: 'www'
+  appName: 'KevinDev',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#5304bf",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: "launch_screen",
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
